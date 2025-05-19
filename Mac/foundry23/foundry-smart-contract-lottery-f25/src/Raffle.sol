c0c95bfbcb9b9b350b9b916a209e853f1b8e2659
WinVerse <winverse@WinVerses-MacBook-Pro.local>
2025-05-19 01:47:47 +0100

    chore: forge init

---
+++ Mac/foundry23/foundry-smart-contract-lottery-f25/src/Raffle.sol
@@ -0,0 +1,92 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts@1.3.0/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+
+
+/**
+ * @title A sample raffle contract
+ * @author WinVerse
+ * @notice This contract is for creating a sample raffle
+ * @dev Implements Chainlink VRFv2.5
+ */
+
+contract Raffle {
+    /* Errors */
+    error Raffle__SendMoreToEnterRaffle();
+
+    uint256 private immutable i_entranceFee; //state variable
+    // @dev The duration of the lottery in seconds
+    uint256 private immutable i_interval;
+    address payable [] private s_players; // can't make it immutable or constant cause no of players will always change
+    uint256 private s_lastTimeStamp;
+
+    /* Events */
+    event RaffleEntered(address indexed player);
+
+    constructor(uint256 entranceFee, uint256 interval) {
+        i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
+    }
+
+    function enterRaffle() external payable {
+        // require(msg.value >= i_entranceFee, "Not enough ETH sent!");
+        // require(msg.value >= i_entranceFee, Raffle__SendMoreToEnterRaffle());
+        if (msg.value < i_entranceFee) {
+            revert Raffle__SendMoreToEnterRaffle();
+        }
+        s_players.push(payable(msg.sender));            //need "payable" in order for an address to receive ETH
+        emit RaffleEntered(msg.sender);                 //anytime you update storage, you want to emit an event
+    }
+
+    // 1. Get a random number 
+    // 2. Use random number to pick a player 
+    // 3. Be automatically called
+    function pickWinner() external {
+        // check to see if enough time has passed
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
+            revert();
+        }
+        requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: s_keyHash,
+                subId: s_subscriptionId,
+                requestConfirmations: requestConfirmations,
+                callbackGasLimit: callbackGasLimit,
+                numWords: numWords,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+                )
+            })
+        );
+    }
+    
+    /** Getter Functions */
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
+}
